191bab45383cf1d71773ee1c5e0db3821e958396
roy.lee <2circumflex@gmail.com>
2020-04-23 00:20:04 +0900

    Add avatar component size

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -2,6 +2,13 @@
 import { swiss } from '@theme-ui/presets'
 export default {
   ...swiss,
+  images: {
+    avatar: {
+      width: 50,
+      height: 50,
+      borderRadius: 25
+    },
+  },
   styles: {
     ...swiss.styles,
   },
